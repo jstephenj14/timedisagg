94b6d01f5dc1ae342b89c0551cccded3c91bfc64
aruls001 <aruls001@umn.edu>
2020-05-15 15:14:29 -0500

    Initial commit

---
+++ src/sympy_build.py
@@ -0,0 +1,27 @@
+
+import sympy as sym
+
+import sympy
+from scipy.optimize import minimize
+from sympy.utilities.lambdify import lambdify
+
+
+x, y = sym.symbols('x, y')
+A = sym.Matrix([[x-2, 0], [0, x-3]])
+func = A.det().as_poly().as_expr()
+my_func = lambdify((x),func)
+results = minimize(my_func,[0.1])
+
+
+from scipy import optimize
+def f(x):
+    deter_A = A.det()
+    return deter_A.as_poly().as_expr()
+
+result = optimize.minimize_scalar(f)
+
+
+a, b, G = sympy.symbols('a b G')
+func = (G - a) ** 2 + b
+my_func = lambdify((G, a, b), -1 * func)
+results = minimize(my_func, [0.1, 0.1, 0.1])
