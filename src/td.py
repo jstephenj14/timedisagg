0755896144888529358621571389300f6fd1b65c
aruls001 <aruls001@umn.edu>
2020-05-23 21:31:28 -0500

    Expected input/output settled

--- src/td.py
+++ src/td.py
@@ -1,32 +1,88 @@
 import pandas as pd
 import numpy as np
-import math
 from scipy.linalg import toeplitz
 import scipy.linalg as linalg
 from scipy.linalg import solve_triangular
 from scipy.optimize import minimize
 from scipy.optimize import Bounds
 import sys
+import math
+
 
 class TempDisagg:
 
-    def __init__(self, conversion, fr, n_bc, n_fc, method="chow-lin-maxlog", rho_start=0.5, truncated_rho=0,
+    def __init__(self, conversion, method="chow-lin-maxlog", rho_start=0.5, truncated_rho=0,
                  fixed_rho=0.5):
         self.conversion = conversion
-        self.fr = fr
-        self.n_bc = n_bc
-        self.n_fc = n_fc
         self.rho_start = rho_start
         self.method = method
         self.fixed_rho = fixed_rho
-        self.n_l = None
         self.truncated_rho = truncated_rho
 
+
+        self.n_l = None
+        self.fr = None
+        self.n_bc = None
+        self.n_fc = None
         self.rho_min = None
+
+    def extract_params(self, df):
+
+        # Sort is mandatory here to preserve order. CRUCIAL
+        df = df.sort_values(["index", "grain"])
+
+        X = np.asarray(df["X"])
+        # Will not accept data if X has nulls. Impute and then send back
+
+        if np.isnan(X).sum() > 0:
+            sys.exit("X has erraneous Nan's. Please impute missing values")
+
+        fr = len(np.unique(df.grain))
+
+        # y can have Nans, since periods can be backcasted or forecasted.
+        # So we'll have to count periods of forecasts and backcasts
+
+        y_check = np.asarray(df["y"])
+
+        n_bc = 0
+        n_fc = 0
+        for i, val in enumerate(y_check):
+            if not math.isnan(val):
+                n_bc = i
+                break
+        for i, val in enumerate(np.flip(y_check)):
+            if not math.isnan(val):
+                n_fc = i
+                break
+
+        # filter out the NAs for y and create y_temp
+        nan_mask = np.ones(len(y_check), np.bool)
+        nan_indexes = np.concatenate([np.arange(n_bc), np.arange(len(y_check) - n_fc, len(y_check), 1)])
+        nan_mask[nan_indexes] = 0
+
+        y = pd.unique(y_check[nan_mask])
+
+        # check for erraneous nan's
+        if np.isnan(y).sum() > 0:
+            sys.exit("y has erraneous Nan's. Please impute missing values")
+
+        return X, y, fr, n_fc, n_bc
 
     def generate_conversion_matrix(self):
 
-        conversion_weights = np.repeat(1, self.fr).reshape((1, self.fr))
+        if self.conversion ==  "sum":
+            conversion_weights = np.repeat(1, self.fr).reshape((1, self.fr))
+        elif self.conversion == "average":
+            conversion_weights = (np.repeat(1, self.fr)/self.fr).reshape((1, self.fr))
+        elif self.conversion == "first":
+            conversion_weights = np.zeros(self.fr).reshape((1, self.fr))
+            conversion_weights[0, 0] = 1
+        elif self.conversion == "last":
+            conversion_weights = np.zeros(self.fr).reshape((1, self.fr))
+            conversion_weights[0,self.fr-1] = 1
+        else:
+            sys.exit("Wrong Conversion")
+
         diagonal_identity = np.identity(self.n_l)
         conversion_matrix = np.kron(diagonal_identity, conversion_weights.T).T
 
@@ -65,12 +121,12 @@ class TempDisagg:
         n = len(X)
         diag = np.identity(n)
         diag_rho = self.fill_off_diag(diag, -rho)
-        # print(X.shape)
-        # print(np.linalg.inv(diag_rho).shape)
-        # print(np.hstack((rho, np.zeros(n-1))).shape)
-        # print(np.hstack((rho, np.zeros(n-1))).reshape(n, 1).shape)
-        # print(np.hstack((X.reshape(n,1), np.hstack((rho, np.zeros(n-1))).reshape(n, 1))).shape)
-        return np.linalg.inv(diag_rho).dot(np.hstack((X.reshape(n,1), np.hstack((rho, np.zeros(n-1))).reshape(n, 1))))
+        # # print(X.shape)
+        # # print(np.linalg.inv(diag_rho).shape)
+        # # print(np.hstack((rho, np.zeros(n-1))).shape)
+        # # print(np.hstack((rho, np.zeros(n-1))).reshape(n, 1).shape)
+        # # print(np.hstack((X.reshape(n,1), np.hstack((rho, np.zeros(n-1))).reshape(n, 1))).shape)
+        return np.linalg.inv(diag_rho).dot(np.hstack((X.reshape(n, 1), np.hstack((rho, np.zeros(n-1))).reshape(n, 1))))
 
     def CalcGLS(self, y, X, vcov, stats=False):
 
@@ -81,9 +137,9 @@ class TempDisagg:
         m = A.shape[0]
         n = A.shape[1]
 
-        print("inside CalcGLS")
-        print(m)
-        print(n)
+        # print("inside CalcGLS")
+        # print(m)
+        # print(n)
 
         B = np.linalg.cholesky(W)
 
@@ -117,10 +173,10 @@ class TempDisagg:
         z = dict()
 
         if len(c_bB1) > 1:
-            # print( " c_bB1.reshape(len(c_bB1), 1)")
-            # print( c_bB1.reshape(len(c_bB1), 1) )
-            # print(" C_bB1.dot(v)")
-            # print( C_bB1.dot(v))
+            # # print( " c_bB1.reshape(len(c_bB1), 1)")
+            # # print( c_bB1.reshape(len(c_bB1), 1) )
+            # # print(" C_bB1.dot(v)")
+            # # print( C_bB1.dot(v))
             c_bB1_reshaped = c_bB1.reshape(len(c_bB1), 1)
             C_bB1_dot_v = C_bB1.dot(v)
 
@@ -129,8 +185,8 @@ class TempDisagg:
         else:
             RHS = c_bB1 - C_bB1.dot(v)
 
-        print("R")
-        print(R)
+        # print("R")
+        # print(R)
 
         x = solve_triangular(R, RHS)
 
@@ -169,9 +225,13 @@ class TempDisagg:
             z["vcov_inv"] = vcov_inv
 
         self.z = z
+
         return z
 
-    def __call__(self, X, y_l):
+    def __call__(self, df):
+
+        df_copy = df.copy()
+        X, y_l, self.fr, self.n_fc, self.n_bc = self.extract_params(df_copy)
 
         n = len(X)
         self.n_l = len(y_l)
@@ -185,7 +245,7 @@ class TempDisagg:
             def objective_fn(rho):
                 Q = self.calculate_Q(pm, rho)
                 vcov = (c_matrix.dot(Q)).dot(c_matrix.T)
-                print("In objective chin-lin: "+str(X_l.shape))
+                # print("In objective chin-lin: "+str(X_l.shape))
                 return -1 * self.CalcGLS(y_l, X_l, vcov)["logl"]
         elif self.method == "chow-lin-minrss-ecotrim":
             def objective_fn(rho):
@@ -211,7 +271,7 @@ class TempDisagg:
             def objective_fn(rho):
                 X_adj = self.calculate_dyn_adj(X, rho)
                 X_l_adj = c_matrix.dot(X_adj)
-                print("In objective dyn: "+str(X_l_adj.shape))
+                # print("In objective dyn: "+str(X_l_adj.shape))
                 Q = self.calculate_Q(pm, rho)
                 vcov = (c_matrix.dot(Q)).dot(c_matrix.T)
                 return -1 * self.CalcGLS(y_l, X_l_adj, vcov)["logl"]
@@ -242,8 +302,8 @@ class TempDisagg:
             elif self.method in ["chow-lin-fixed", "litterman-fixed", "dynamic-fixed"]:
                 self.rho_min = self.fixed_rho
 
-        print("self.rho_min ")
-        print(self.rho_min)
+        # print("self.rho_min ")
+        # print(self.rho_min)
         if self.method in ["chow-lin-maxlog", "chow-lin-minrss-ecotrim",
                             "chow-lin-minrss-quilis", "chow-lin-fixed",
                             "dynamic-maxlog", "dynamic-minrss",
@@ -254,7 +314,7 @@ class TempDisagg:
 
         if self.method in ["dynamic-maxlog", "dynamic-minrss", "dynamic-fixed"] and self.rho_min != 0:
             X = self.calculate_dyn_adj(X, self.rho_min).reshape(X.shape[0], 2)
-            print("inside if:"+str(X.shape))
+            # print("inside if:"+str(X.shape))
             X_l = c_matrix.dot(X)
         else:
             X = X.reshape(len(X),1)
@@ -262,26 +322,27 @@ class TempDisagg:
         Q_l_real = c_matrix.dot(Q_real).dot(c_matrix.T)
         z = self.CalcGLS(y_l, X_l, Q_l_real, stats=True)
 
-        # print(z["coefficients"].shape)
-        # print(X.reshape(len(X), 2).shape)
-        print("z['coefficients']:")
-        print(z["coefficients"])
+        # # print(z["coefficients"].shape)
+        # # print(X.reshape(len(X), 2).shape)
+        # # print("z['coefficients']:")
+        # # print(z["coefficients"])
 
         p = X.dot(z["coefficients"])
         D = Q_real.dot(c_matrix.T).dot(z["vcov_inv"])
 
         u_l = y_l.reshape(len(y_l), 1) - c_matrix.dot(p).reshape(len(c_matrix.dot(p)),1)
 
-        y = p.reshape(len(p),1) + D.dot(u_l)
+        y = p.reshape(len(p), 1) + D.dot(u_l)
 
-        return y
+        df_copy["y_hat"] = y
 
+        return df_copy
 
-y_l_data = pd.read_csv("C:/Users/jstep/Documents/y_l.csv")
-X_data = pd.read_csv("C:/Users/jstep/Documents/X.csv")
-
-y_l = np.asarray(y_l_data["V1"])
-X = np.asarray(X_data["exports.q"])
+# y_l_data = pd.read_csv("C:/Users/jstep/Documents/y_l.csv")
+# X_data = pd.read_csv("C:/Users/jstep/Documents/X.csv")
+#
+# y_l_prev = np.asarray(y_l_data["V1"])
+# X_prev = np.asarray(X_data["exports.q"])
 
 # td_obj = TempDisagg(conversion="sum", fr=4, n_bc=12, n_fc=2)
 # td_obj(X, y_l)
@@ -291,24 +352,42 @@ X = np.asarray(X_data["exports.q"])
 #
 # td_obj = TempDisagg(conversion="sum", fr=4, n_bc=12, n_fc=2, method="chow-lin-minrss-ecotrim")
 # td_obj(X, y_l)
-# print(td_obj.rho_min)
+# # print(td_obj.rho_min)
 #
 # td_obj = TempDisagg(conversion="sum", fr=4, n_bc=12, n_fc=2, method="chow-lin-minrss-quilis")
-# print(td_obj(X, y_l))
-# print(td_obj.rho_min)
+# # print(td_obj(X, y_l))
+# # print(td_obj.rho_min)
 #
 # td_obj = TempDisagg(conversion="sum", fr=4, n_bc=12, n_fc=2, method="litterman-maxlog")
-# print(td_obj(X, y_l))
-# print(td_obj.rho_min)
+# # print(td_obj(X, y_l))
+# # print(td_obj.rho_min)
 #
 # td_obj = TempDisagg(conversion="sum", fr=4, n_bc=12, n_fc=2, method="litterman-minrss")
-# print(td_obj(X, y_l))
-# print(td_obj.rho_min)
+# # print(td_obj(X, y_l))
+# # print(td_obj.rho_min)
 
 # td_obj = TempDisagg(conversion="sum", fr=4, n_bc=12, n_fc=2, method="dynamic-maxlog")
-# print(td_obj(X, y_l))
-# print(td_obj.rho_min)
+# # print(td_obj(X, y_l))
+# # print(td_obj.rho_min)
+#
+# td_obj = TempDisagg(conversion="sum", fr=4, n_bc=12, n_fc=2, method="dynamic-minrss")
+# # print(td_obj(X, y_l))
+# # print(td_obj.rho_min)
 
-td_obj = TempDisagg(conversion="sum", fr=4, n_bc=12, n_fc=2, method="dynamic-minrss")
-print(td_obj(X, y_l))
-print(td_obj.rho_min)
+
+exports_q = pd.read_csv("C:/Users/jstep/Documents/exports.q.csv")
+exports_q = exports_q.rename(columns={'value':"X"})
+sales_a = pd.read_csv("C:/Users/jstep/Documents/sales.a.csv")
+sales_a = sales_a.rename(columns={'value':"y"})
+
+expected_dataset = pd.merge(exports_q,sales_a,on="index",how="left")
+# grain and index must be numbers
+expected_dataset["grain"] = expected_dataset["grain"].str.replace("Q","").astype("int")
+
+
+for conversion in ["sum","average","first","last"]:
+    td_obj = TempDisagg(conversion=conversion,method="dynamic-minrss")
+    df_output = td_obj(expected_dataset)
+    print(conversion)
+    print(df_output)
+    print(td_obj.rho_min)
